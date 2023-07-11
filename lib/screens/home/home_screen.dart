239c22a8d99925a52b1ebf0dc65ca382266a701d
ericatsu <ekaatsu@st.ug.edu.gh>
2023-07-11 22:41:00 +0000

    ✨ home

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_doctor_booking/screens/home/widgets/top_widget.dart';
+import 'package:mobile_doctor_booking/shared/exports.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final mediaQuery = SizeQuery(context);
+    return Scaffold(
+      body: Column(
+        children: [
+          TopWidget()
+        ],
+      ),
+    );
   }
 }
